2d46bae44fc613fa76671ead2b237d54abd145c7
shan2 <shaniaki@users.noreply.github.com>
2014-12-22 13:50:37 +0000

    f2sdf3: improvements for easier invocation

--- forsyde-to-sdf3.sh
+++ forsyde-to-sdf3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # first argument: the top-level input XML file
 # second argument: the application name
-# third argument: the folder for the input XML
+# third argument: the folder for the input XML with trailing slash
+mkdir -p output
+cp DTD/forsyde.dtd $3
 saxonb-xslt -s:$1 -xsl:converter.xsl -o:output/converter.log -dtd:off -ext:on application-name=$2 inputFolder=$3 outputFolder=output
